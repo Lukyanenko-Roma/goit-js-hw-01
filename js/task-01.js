3c758e929399468d7f195b94fd03b00a478e47ad
Lukyanenko-Roma <70845043+Lukyanenko-Roma@users.noreply.github.com>
2020-12-01 11:21:33 +0200

    Add files via upload

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,10 @@
 
 const name = "Генератор защитного поля";
 let price = 1000;
+const firstProduct = ` Выбран ${name},  цена за штуку ${price} кредитов`;
 
-const product = ` Выбран ${name},  цена за штуку ${price} кредитов`;
+price = 2000;
+const secondProduct = ` Выбран ${name},  цена за штуку ${price} кредитов`;
 
-console.log(product);
+console.log(firstProduct);
+console.log(secondProduct);
